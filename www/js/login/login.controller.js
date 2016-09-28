57ae7f77d49d52730f8559ad45f9d9ff27d9b458
jeagle <joshuaseagle@gmail.com>
2016-09-28 17:17:02 -0400

    Added email authentication.

--- www/js/login/login.controller.js
+++ www/js/login/login.controller.js
@@ -6,11 +6,21 @@ LoginCtrl.$inject = ['$scope', 'loginUtils'];
 function LoginCtrl ($scope, loginUtils) {
     var vm = this;
     
-    $scope.toggleLogin = function (chosenProvider) {
-        loginUtils.toggleLogin(chosenProvider);
+    $scope.toggleLogin = function (chosenProvider, toggleRegistrationForm) {
+        loginUtils.toggleLogin(chosenProvider, toggleRegistrationForm);
     }
 
     $scope.signout = function (chosenProvider) {
         loginUtils.signout(chosenProvider);
+    }
+
+    $scope.toggleSignUp = function ($event) {
+        if (this.toggleRegistrationForm.toggle) {
+            this.toggleRegistrationForm.toggle = false;
+            $event.currentTarget.innerHTML = 'Register';
+        } else {
+            this.toggleRegistrationForm.toggle = true;
+            $event.currentTarget.innerHTML = 'Cancel';
+        }
     }
 }
--- www/js/loginUtils.js
+++ www/js/loginUtils.js
@@ -1,4 +1,3 @@
-'use strict';
 angular.module('unisys.onboarding.loginUtils', [])
     .factory('loginUtils', loginUtils);
 
@@ -34,20 +33,26 @@ function loginUtils () {
             firebase.auth().signOut();
             this.user = {};
         },
-        toggleLogin: function (chosenProvider) {
+        toggleLogin: function (chosenProvider, toggleRegisterationForm) {
             var that = this;
+            
+            chosenProvider = chosenProvider || null;
+            
             if (!firebase.auth().currentUser) {
-                var provider = this.providers[chosenProvider];
 
-                switch (provider.providerId) {
-                    case "google.com":
-                        provider.addScope('https://www.googleapis.com/auth/plus.login');
-                        break;
-                    case "github.com":
-                        provider.addScope('repo');
-                        break;
-                    default: 
-                        provider = null;
+                if (chosenProvider){
+                    var provider = this.providers[chosenProvider];
+
+                    switch (provider.providerId) {
+                        case "google.com":
+                            provider.addScope('https://www.googleapis.com/auth/plus.login');
+                            break;
+                        case "github.com":
+                            provider.addScope('repo');
+                            break;
+                        default: 
+                            provider = null;
+                    }
                 }
 
                 if (provider) {
@@ -68,7 +73,7 @@ function loginUtils () {
                             console.error(error);
                         }
                     });
-                } else {
+                } else if (!toggleRegisterationForm) {
                     var email = document.getElementById('email').value;
                     var password = document.getElementById('password').value;
 
@@ -84,10 +89,63 @@ function loginUtils () {
                         }
                         console.log(error);
                     });
+                } else {
+                    this.handleSignUp(toggleRegisterationForm);
                 }
                 return true;
             }
             return false;
+        },
+        handleSignUp: function (toggleRegistrationForm) {
+            var email = document.getElementById('email').value;
+            var password = document.getElementById('password').value;
+            var confirmEmail = document.getElementById('confirm-email').value;
+            var confirmPassword = document.getElementById('confirm-password').value;
+            if (toggleRegistrationForm) {
+                if (email.toLowerCase() !== confirmEmail.toLowerCase()) {
+                    return;
+                }
+
+                if (password.value !== confirmPassword.value) {
+                    return;
+                }
+            }
+
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                if (errorCode == 'auth/weak-password') {
+                    alert('The password is too weak.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
+        },
+        sendEmailVerification: function () {
+            firebase.auth().currentUser.sendEmailVerification().then(function() {
+                alert('Email Verification Sent!');
+            });
+        },
+        sendPasswordReset: function () {
+            var email = document.getElementById('email').value;
+            
+            firebase.auth().sendPasswordResetEmail(email).then(function() {
+                
+                alert('Password Reset Email Sent!');
+
+            }).catch(function(error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                if (errorCode == 'auth/invalid-email') {
+                    alert(errorMessage);
+                } else if (errorCode == 'auth/user-not-found') {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
         }
     };
     utils.initialize();
