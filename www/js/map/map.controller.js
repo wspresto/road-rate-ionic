a32ebc447df01f61bf5e1849ee1565b75f8e2a39
wspresto <wpreston@synergybis.com>
2016-09-29 15:54:23 -0400

    map centers and prevents panning on current location retrieved by geolocator plugin.

--- www/js/map/map.controller.js
+++ www/js/map/map.controller.js
@@ -5,21 +5,24 @@ RoadCtrl.$inject = ['$scope', '$ionicPlatform', '$ionicActionSheet', 'esriRegist
 
 function RoadCtrl ($scope, $ionicPlatform, $ionicActionSheet, esriRegistry, $timeout, esriService, googleMapsService, MOCK, $cordovaGeolocation) {
     var vm = this;
+    var zoomLevel = 18;    
+    var voteConfirmationDelay = 5 * 1000;
     vm.downVote = downVote;
     vm.upVote = upVote;
-    vm.voteConfirmationDelay = 5 * 1000;
+
     vm.gpsPromise = null;
     vm.map = {
+        controller: null,
         options: {
             basemap: 'streets',
             center: [MOCK.LONGITUDE, MOCK.LATITUDE],
-            zoom: 18,
+            zoom: zoomLevel,
             sliderStyle: 'small',
             logo: false
         }
     };
     vm.road = {
-        short_name: 'Discovering Road..',
+        short_name: 'Discovering Road..'
     };
     vm.postal = {
         long_name: ''
@@ -30,38 +33,54 @@ function RoadCtrl ($scope, $ionicPlatform, $ionicActionSheet, esriRegistry, $tim
         var close = $ionicActionSheet.show({     
             titleText: 'You like this road.',
         });
-        $timeout(close, vm.voteConfirmationDelay); 
+        $timeout(close, voteConfirmationDelay); 
     }
     function upVote () {
         var close = $ionicActionSheet.show({    
             titleText: 'You hate this road.',
         });
-        $timeout(close, vm.voteConfirmationDelay); 
+        $timeout(close, voteConfirmationDelay); 
     }
     function updateLocation (position) {
-            googleMapsService.discoverRoad(position.coords.latitude, position.coords.longitude).then(function (details) {
-                vm.road = details.road;
-                vm.postal = details.postal;
-            });
+        esriService.loadModule('esri/geometry/Point').then(function (Point) {
+            var pt = new Point(position.coords.longitude, position.coords.latitude);
+            $timeout(function () {vm.map.controller.centerAndZoom(pt, zoomLevel)}, 0); //do not run during $digest
+        });
+        googleMapsService.discoverRoad(position.coords.latitude, position.coords.longitude).then(function (details) {
+            vm.road = details.road;
+            vm.postal = details.postal;
+        });
     }
     function init () {
         $ionicPlatform.ready(function() {
-            //fetch gps coordinates
-            vm.gpsPromise = $cordovaGeolocation.watchPosition({
-                enableHighAccuracy: false,
-                timeout: 3000
-            });
-            vm.gpsPromise.then(null, function (err) {
+            esriRegistry.get('roadMap').then(function (map) {
+                map.on("load", function() {
+                    vm.map.controller = map;
+                    map.disableRubberBandZoom();
+                    map.disableShiftDoubleClickZoom();
+                    map.disableClickRecenter();
+                    map.disableDoubleClickZoom();
 
-            }, updateLocation);
+                    map.disableScrollWheelZoom();      
+                    map.disableSnapping();              
+                    map.disablePan();
+                    map.disableMapNavigation();                    
+                    map.hideZoomSlider();
+
+
+                    //fetch gps coordinates
+                    vm.gpsPromise = $cordovaGeolocation.watchPosition({
+                        enableHighAccuracy: false,
+                        timeout: 3000
+                    });
+                    vm.gpsPromise.then(null, function (err) {
+
+                    }, updateLocation);                    
+                });
+            });            
+
         });        
-        esriRegistry.get('roadMap').then(function (map) {
-            map.on("load", function() {
-                map.disablePan();
-                map.hideZoomSlider();
-                map.disableScrollWheelZoom();
-            });
-        });
+
 
     }    
 }
